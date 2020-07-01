9b42ac0ad4006cb7fc65adfb903b1387d53e0735
kndt <mnxx@protonmail.ch>
2020-07-01 13:05:25 +0100

    added wip none fileds pkg

--- non_none_fields/src/lib.rs
+++ non_none_fields/src/lib.rs
@@ -1,7 +1,8 @@
-use proc_macro::TokenStream;
-use quote::quote;
+use proc_macro::{TokenStream};
+use quote::{quote};
 use syn::Path;
-use syn::{parse_macro_input, Type, ItemStruct };
+use syn;
+use syn::{parse_macro_input, Type};
 
 fn path_is_not_none(path: &Path) -> bool {
         path.leading_colon.is_none()
@@ -12,7 +13,9 @@ fn path_is_not_none(path: &Path) -> bool {
 #[proc_macro_derive(NotNoneFields)]
 pub fn non_none_fields(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
-    let input = parse_macro_input!(input as ItemStruct);
+    let input = parse_macro_input!(input as &syn::ItemStruct);
+    // let item: syn::Item = syn::parse(input).expect("failed to parse input");
+
 
     let name = &input.ident;
 
@@ -20,45 +23,40 @@ pub fn non_none_fields(input: TokenStream) -> TokenStream {
     //   if field.ty != TypeNever { field.ident }
     // });
 
-    let not_none_field_names: Vec<String>;
-    let mut array: [String; 10];
+    // impl ToTokens for Vec {
+    //     fn to_tokens(&self, tokens: &mut TokenStream) {
+    //         let d = self.0.iter().map(|datum| datum.0);
+    //         tokens.extend(quote! {
+    //             Data(<[Datum]>::into_vec(Box::new([ #( Datum(#d) ),* ])))
+    //         })
+    //     }
+    // }
+    // let not_none_field_names: Vec<String>;
+    // TokenStream::from_iter(vec![
+    //     TokenTree::from(Ident::new("let", span)),
+    //     TokenTree::from(Ident::new("field_names", span)),
+    // ]);
+    let mut field_names: Vec<String> = Vec::new();
     for field in input.fields.iter() {
         // if field.ty != None { not_none_field_names.push(stringify!(field.ident).to_string()) }
 
-        match field.ty {
+        match &field.ty {
             Type::Path(typepath) if typepath.qself.is_none() && path_is_not_none(&typepath.path) => {
-                not_none_field_names.push(stringify!(field.ident).to_string())
-            }
+                field_names.push(stringify!(field.ident).to_string());
+            },
+            _ => (),
         }
     }
 
     // Build the output
     let output = quote! {
         impl #name {
-            pub fn not_none_fields() -> Vec<String> {
-                #not_none_field_names
+            pub fn not_none_fields() -> [String]{
+                #(field_names)
             }
         }
     };
 
     // Hand the output tokens back to the compiler
     TokenStream::from(output)
-}
-
-#[cfg(test)]
-mod tests {
-
-    #[derive(NotNoneFields)]
-    struct SampleTest {
-        first: str,
-        sec: Option<u8>,
-        thr: Option<u8>
-    }
-
-    #[test]
-    fn it_works() {
-        let samp = SampleTest("hellp", Some(1), None);
-        let nn_flds = samp.non_none_fields();
-        assert_eq!(2 + 2, 4);
-    }
 }
