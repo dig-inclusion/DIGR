596e1ddae037bbfb4468de5bf8f7803c9dcb1f19
kndt <mnxx@protonmail.ch>
2020-08-13 16:50:58 +0100

    changed flag rules type

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::string::String;
 use std::fs::File;
 use std::path::PathBuf;
+use std::result::Result::Err;
+use std::fs::metadata;
 use structopt::StructOpt;
 use smol;
 use surf;
 use anyhow::{Error};
-use scraper::{Html, Selector};
+use scraper::{Html};
 mod rules_spec;
+mod rules_path;
 mod test_fns;
 
 #[derive(StructOpt, Debug)]
@@ -15,8 +18,8 @@ mod test_fns;
     about = "An automated accessibility test runner based on rules"
 )]
 struct Arguments {
-    #[structopt(short = "r", long = "rules", help = "Rules folder")]
-    rules: PathBuf,
+    #[structopt(short = "r", long = "rules", help = "Rules file or folder")]
+    rules: String,
 
     #[structopt(short = "u", long = "url", help = "Url to test")]
 	url: String,
@@ -25,13 +28,18 @@ struct Arguments {
 	depth: u8,
 }
 
-fn main() {
+fn main(){
     let opts = Arguments::from_args();
     let site_url: &str = &opts.url;
 
-    let file = File::open(opts.rules).expect("Unable to open file, please remember file or folder argument with the -f option.");
+    let md = metadata(opts.rules.clone()).unwrap();
+    let is_file = md.is_file();
+    let is_dir = md.is_dir();
+
+    let file = File::open(opts.rules).expect("Unable to open file, please remember file or folder argument with the -r option.");
     let spec: rules_spec::RuleSpec = serde_yaml::from_reader(file).expect("There was an error parsing rules file.");
-	
+    let r :&str = &opts.rules;
+
 	smol::run(async {
 		let body = surf::get(site_url)
 			.recv_string().await
@@ -43,20 +51,22 @@ fn main() {
 		};
 		let page_slice: &str = &b;
         let fragment = Html::parse_fragment(page_slice);
-        
-        for tag in spec.on.iter() {
-            let selector = match Selector::parse(tag) {
-                Ok(s) => s,
-                Err(_) => Selector::parse("h3.nil").unwrap() // this isn't ideal but a work around
-            };
-            let test_result = spec.rules_ops(&selector, &fragment);
-            println!("{}", tag);
+        let rules_path = PathBuf::from(r);
+
+        if is_file {
+c           let test_result = rules_path::file_op(rules_path, &spec.on, &fragment).await;
+            for res_op in test_result.iter() {
+                println!("{:?}", res_op);
+            }
+        }
+
+        if is_dir {
+            let test_result = rules_path::folder_op(rules_path, &spec.on, &fragment).await;
             for res_op in test_result.iter() {
                 println!("{:?}", res_op);
             }
         }
 	});
-
 }
 
 
--- src/rules_path.rs
+++ src/rules_path.rs
@@ -1,4 +1,5 @@
-use std::result{Error, Result};
+use std::result::{Result};
+use std::io::Error;
 use std::string::String;
 use std::fs::{File, read_dir};
 use std::path::PathBuf;
@@ -18,21 +19,17 @@ pub async fn file_op(rules_file: PathBuf, tags: &Vec<String>, fragment: &Html) -
         };
         let op_res = spec.rules_ops(&selector, &fragment);
         test_results.extend(op_res);
-        // println!("{}", tag);
-        // for res_op in test_result.iter() {
-        //     println!("{:?}", res_op);
-        // }
     }
     return Ok(test_results);
 }
 
 /// Sends a request and fetches the response.
 pub async fn folder_op<'a>(rules_folder: PathBuf, tags: &Vec<String>, fragment: &Html) -> Result<Vec<rules_spec::OpsResult>, Error> {
-    let mut test_results: Vec<rules_spec::OpsResult>;
+    let mut test_results: Vec<rules_spec::OpsResult> = vec![];
     for entry in read_dir(rules_folder)? {
         let entry = entry?;
         let path = entry.path();
-        let res = file_op(path, &tags, &fragment).await;
+        let res = file_op(path, &tags, &fragment).await?;
         test_results.extend(res);
     }
     return Ok(test_results);
